ea6d5619eff4a7c0b3601dee28bf39485c8a7edb
Ashutosh730 <Ashu@730>
2020-05-20 23:20:35 +0530

    Implemented user's tab

--- app/src/main/java/com/example/instaclone/PicTab.java
+++ app/src/main/java/com/example/instaclone/PicTab.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,6 +15,10 @@ import androidx.fragment.app.Fragment;
  * A simple {@link Fragment} subclass.
  */
 public class PicTab extends Fragment {
+
+    private ImageView road;
+    private EditText description;
+    private Button share;
 
     public PicTab() {
         // Required empty public constructor
@@ -22,6 +29,14 @@ public class PicTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pic_tab, container, false);
+        View v= inflater.inflate(R.layout.fragment_pic_tab, container, false);
+
+        road=v.findViewById(R.id.road);
+        description=v.findViewById(R.id.description);
+        share=v.findViewById(R.id.share);
+
+
+
+        return  v;
     }
 }
