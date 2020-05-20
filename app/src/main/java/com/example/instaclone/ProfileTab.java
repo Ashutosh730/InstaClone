329ae69f610a9cf3b3dd4746812f466df6dd04f9
Ashutosh730 <Ashu@730>
2020-05-20 22:59:11 +0530

    Implemented user's tab

--- app/src/main/java/com/example/instaclone/ProfileTab.java
+++ app/src/main/java/com/example/instaclone/ProfileTab.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+
+    private EditText name,bio,sport,hobbies,profession;
+    private Button update;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -22,6 +32,70 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        name = v.findViewById(R.id.name);
+        bio = v.findViewById(R.id.bio);
+        sport = v.findViewById(R.id.sport);
+        hobbies = v.findViewById(R.id.hobbies);
+        profession = v.findViewById(R.id.profession);
+        update = v.findViewById(R.id.update);
+
+        final ParseUser user=ParseUser.getCurrentUser();
+
+        if(user.get("profileName")!=null)
+            name.setText(user.get("profileName")+"");
+        else
+            name.setHint("Nothing entered,please update");
+
+        if(user.get("profilebio")!=null)
+            bio.setText(user.get("profilebio")+"");
+        else
+            bio.setHint("Nothing entered,please update");
+
+        if(user.get("profilesport")!=null)
+        sport.setText(user.get("profilesport")+"");
+        else
+            sport.setHint("Nothing entered,please update");
+
+            if(user.get("profilehobbies")!=null)
+        hobbies.setText(user.get("profilehobbies")+"");
+        else
+        hobbies.setHint("Nothing entered,please update");
+
+        if(user.get("profileprofession")!=null)
+        profession.setText(user.get("profileprofession")+"");
+        else
+        profession.setHint("Nothing entered,please update");
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user.put("profileName",name.getText().toString());
+                user.put("profilebio",bio.getText().toString());
+                user.put("profilesport",sport.getText().toString());
+                user.put("profilehobbies",hobbies.getText().toString());
+                user.put("profileprofession",profession.getText().toString());
+
+                user.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if(e==null)
+                        {
+                            Toast.makeText(getContext(), "Your data is saved", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/example/instaclone/UsersTab.java
+++ app/src/main/java/com/example/instaclone/UsersTab.java
@@ -4,24 +4,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UsersTab extends Fragment {
 
-    public UsersTab() {
-        // Required empty public constructor
-    }
+    private ListView list;
+    private ArrayList arraylist;
+    private ArrayAdapter arrayAdapter;
 
+    public UsersTab() {
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        View v= inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        list=v.findViewById(R.id.list);
+        arraylist=new ArrayList();
+        arrayAdapter=new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arraylist);
+        final TextView txt=v.findViewById(R.id.txt);
+
+        ParseQuery<ParseUser> query=ParseUser.getQuery();
+
+        query.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+
+                if(e==null)
+                {
+                    if(objects.size()>0)
+                    {
+                        for(ParseUser user:objects)
+                        {
+                            arraylist.add(user.getUsername());
+                        }
+
+                        list.setAdapter(arrayAdapter);
+                        txt.animate().alpha(0).setDuration(500);
+                        list.setVisibility(View.VISIBLE);
+
+                    }
+
+                }
+
+            }
+        });
+
+        return v;
     }
 }
