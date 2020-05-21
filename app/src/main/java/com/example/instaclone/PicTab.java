bf6935dea7c084933956ff47718f29cfa9356d12
Ashutosh730 <Ashu@730>
2020-05-21 12:56:48 +0530

    Implemented share picture tab

--- app/src/main/java/com/example/instaclone/PicTab.java
+++ app/src/main/java/com/example/instaclone/PicTab.java
@@ -1,24 +1,47 @@
 package com.example.instaclone;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PicTab extends Fragment {
+public class PicTab extends Fragment implements View.OnClickListener{
 
-    private ImageView road;
+    private ImageView img;
     private EditText description;
     private Button share;
+    Bitmap receivedImageBitmap;
 
     public PicTab() {
         // Required empty public constructor
@@ -31,12 +54,111 @@ public class PicTab extends Fragment {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_pic_tab, container, false);
 
-        road=v.findViewById(R.id.road);
+        img=v.findViewById(R.id.img);
         description=v.findViewById(R.id.description);
         share=v.findViewById(R.id.share);
 
-
+        img.setOnClickListener(PicTab.this);
+        share.setOnClickListener(PicTab.this);
 
         return  v;
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.img:
+                if (android.os.Build.VERSION.SDK_INT >= 23
+                        && ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+                } else {
+                    getChosenImage();
+                }
+                break;
+
+            case R.id.share:
+                if (receivedImageBitmap != null) {
+                        if (description.getText().toString().equals("")) {
+                            Toast.makeText(getContext(), "Error Describe some description", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        receivedImageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                        byte[] bytes = byteArrayOutputStream.toByteArray();
+                        ParseFile parseFile = new ParseFile("img.png", bytes);
+                        ParseObject parseObject = new ParseObject("Photo");
+                        parseObject.put("picture", parseFile);
+                        parseObject.put("image_des", description.getText().toString());
+                        parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                        final ProgressDialog dialog=new ProgressDialog(getContext());
+                        dialog.setMessage("Loading...");
+                        dialog.show();
+                        parseObject.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    Toast.makeText(getContext(), "Done!!!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getContext(), "Unknown error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+                }
+        }
+
+
+    private void getChosenImage() {
+
+        Intent intent=new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,2000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+
+        if(requestCode==1000){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getChosenImage();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == 2000){
+            if(resultCode== Activity.RESULT_OK){
+                try{
+                    Uri selectedImage=data.getData();
+                    String[] filePathColumn={MediaStore.Images.Media.DATA};
+                    Cursor cursor=getActivity().getContentResolver().query(selectedImage,filePathColumn,
+                            null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex=cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath=cursor.getString(columnIndex);
+                    cursor.close();
+                    receivedImageBitmap = BitmapFactory.decodeFile(picturePath);
+                    img.setImageBitmap(receivedImageBitmap);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
